7651adf8b3fbc395c880ee68594943109598f085
Yug Gurnani <yug.gurnani091@gmail.com>
2024-04-11 15:56:27 +0530

    Refactored

--- leap_year.rb
+++ leap_year.rb
@@ -1,9 +1,7 @@
 class LeapYear
   def self.check(year)
-    return false if year%4 != 0
+    return true if year%4 == 0 && (year%100 != 0 || year%400 == 0)
 
-    return false if year%100 == 0 && year%400 != 0
-
-    true
+    false
   end
 end
