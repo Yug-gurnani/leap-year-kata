75685cf2c8b084a0aa7e5feb4c9fb3dd92ce6159
Yug Gurnani <yug.gurnani091@gmail.com>
2024-04-11 15:30:12 +0530

    Handled cases
    Where years are divisible by 100 and not by 400

--- leap_year.rb
+++ leap_year.rb
@@ -1,5 +1,7 @@
 class LeapYear
   def self.check(year)
+    return false if year%100 == 0 && year%400 != 0
+    
     true
   end
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -7,5 +7,15 @@ describe LeapYear do
         expect(LeapYear.check(2000)).to be true
       end
     end
+
+    context 'Years divible by 100 and not by 400' do
+      it 'should return false when year is 1800' do
+        expect(LeapYear.check(1800)).to be false
+      end
+
+      it 'should return false when year is 2100' do
+        expect(LeapYear.check(2100)).to be false
+      end
+    end
   end
 end
