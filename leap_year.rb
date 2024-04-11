9d5066bc2fb15d95877ed1c1011e9723bdf7c121
Yug Gurnani <yug.gurnani091@gmail.com>
2024-04-11 15:36:00 +0530

    Handled Cases
    Where year is not divisible by 4

--- leap_year.rb
+++ leap_year.rb
@@ -1,7 +1,9 @@
 class LeapYear
   def self.check(year)
+    return false if year%4 != 0
+
     return false if year%100 == 0 && year%400 != 0
-    
+
     true
   end
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -27,5 +27,15 @@ describe LeapYear do
         expect(LeapYear.check(2012)).to be true
       end
     end
+
+    context 'Years not divisible by 4' do
+      it 'should return false when year is 2017' do
+        expect(LeapYear.check(2017)).to be false
+      end
+
+      it 'should return false when year is 2018' do
+        expect(LeapYear.check(2018)).to be false
+      end
+    end
   end
 end
